8d505d86466149c2e22cafdcda4a00885db5686d
AKO1991 <39424758+AKO1991@users.noreply.github.com>
2025-11-07 10:23:38 +0530

    Create stored procedure for loading bronze layer
    Added detailed comments and structure for the stored procedure to load data into the bronze layer.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,5 +1,22 @@
--- Creating Stored Procedure for Full Load operation in Bronze layer
-----------------------------------------------------------------------------
+/*
+==============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+==============================================================================
+
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files.
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from CSV files into bronze tables.
+
+Parameters:
+    None.
+    This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+==============================================================================
+*/
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 DECLARE @start_time DATETIME, @end_time DATETIME;
